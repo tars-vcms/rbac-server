49a9f90a177e8cefce55e65a8241038eb652638a
DRC <caiwenzhe2021@gmail.com>
2021-07-08 00:05:46 +0800

    fix: DSN赋值错误

--- repo/mysql/api.go
+++ repo/mysql/api.go
@@ -8,8 +8,10 @@ var db *gorm.DB
 var dbDsn string
 
 type AccessPoint interface {
-
-
+	Create(_ident string, _comment string)(AccessPointModel, int, error)
+	Update(_id int, _ident string, _comment string)(AccessPointModel, int, error)
+	Read(_where string, _pageSize int, _offset int)([]AccessPointModel, int, error)
+	Delete(_id int)(AccessPointModel, int, error)
 }
 
 
